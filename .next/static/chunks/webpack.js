1ed12434f5e57033aeb2cd14fa090840c8b2dcde
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2025-06-24 14:47:08 -0700

    Logo fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "323a3406343615a3"; }
+/******/ 		__webpack_require__.h = function() { return "c6a718801d0bb9e6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
